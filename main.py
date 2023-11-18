3ac32fd74421bba12f6c48b3a97edcebab3b3494
0101dusica <trbovicdusica@gmail.com>
2023-11-18 12:55:48 -0800

    filter added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 import json
 from flask import Flask, jsonify
 from loadAll import loadAllData
-from prints import terminal_print
+from filter import *
 
 app = Flask(__name__)
 player_data = None
@@ -25,12 +25,14 @@ def load_data(file_path):
 def get_stats():
     global player_data
     if player_data is None:
-        player_data = load_data('L9HomeworkChallengePlayersInput.csv')
+        player_data = load_data('L9HackathonPlayersInput.csv')
 
     player_stats = loadAllData(player_data)
+    #print("ovo sam dobio kao resenje ", player_stats)
+    new = get_candidates_for_national_team(player_stats)
 
     # Assuming player_stats is a list of dictionaries
-    response = jsonify(player_stats)
+    response = jsonify(new)
     
     return response  # Return the response object directly
 
